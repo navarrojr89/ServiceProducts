5a2e66d450be1efd758462160cb8e3cb00584f6f
Luis Navarro <navarrojr89@gmail.com>
2021-02-12 17:55:20 +0100

    Starts the ServiceProduct project

---
+++ src/main/java/com/lamn/microservices/serviceproducts/ServiceProductsApplication.java
@@ -0,0 +1,13 @@
+package com.lamn.microservices.serviceproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProductsApplication.class, args);
+	}
+
+}
