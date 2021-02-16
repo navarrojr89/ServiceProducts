824a295fcd9233572dc1e6e0fe62f9b09b268616
Luis Navarro <navarrojr89@gmail.com>
2021-02-16 16:54:49 +0100

    Implements the Edit and Delete on the ProductController

--- src/main/java/com/lamn/microservices/serviceproducts/controllers/ProductController.java
+++ src/main/java/com/lamn/microservices/serviceproducts/controllers/ProductController.java
@@ -4,9 +4,11 @@ import com.lamn.microservices.serviceproducts.models.entity.Product;
 import com.lamn.microservices.serviceproducts.models.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +55,21 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public Product createProduct(@RequestBody Product product) {
         return productService.save(product);
+    }
+
+    @PutMapping("/edit/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product editProduct(@RequestBody Product newProduct, @PathVariable Long id) {
+        Product currentProduct = productService.findById(id);
+        currentProduct.setName(newProduct.getName());
+        currentProduct.setPrice(newProduct.getPrice());
+        return productService.save(currentProduct);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteById(id);
     }
 
 }
